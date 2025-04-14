b54a08c1a5f641e4ce93ec8b799b33a03543fedb
Nsohal0614 <nidhisohal@hotmail.com>
2025-04-13 21:55:18 -0400

    file handling assignments

--- highlowaverage.py
+++ highlowaverage.py
@@ -7,3 +7,25 @@
 #Highest number
 #Lowest number
 #Correct answers for the included file:
+
+file = open("numbers.txt", "r")
+
+numbers = []
+for line in file:
+        numbers.append(int(line.strip()))
+
+file.close()
+
+count = len(numbers)
+total = sum(numbers)
+average = total / count
+highest = max(numbers)
+lowest = min(numbers)
+
+print("How many numbers in the file:", count)
+print("Total of all the numbers:", total)
+print("Average:", average)
+print("Highest number:", highest)
+print("Lowest number:", lowest)
+
+
--- palindrome.py
+++ palindrome.py
@@ -3,3 +3,16 @@
 #For example, wow is a paldindrome.
 #A different file wile be used for grading.
 #Correct answer for this file: 
+
+file = open("words.txt", "r")
+
+palindrome = 0
+
+for line in file: 
+    word = line.strip()
+    if word == word[::-1]:
+        palindrome += 1
+
+file.close()
+
+print("Number of palindromes:", palindrome)
